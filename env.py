7cb444f70e4edff43da5cf13533c70de95cafe9c
Krasi5 <krasi_todorova@yahoo.com>
2025-12-03 01:15:41 +0000

    slash added

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_xlY0MO8yXcPf@ep-green-sound-ag3a4c5n.c-2.eu-central-1.aws.neon.tech/voter_bust_crawl_761342"
+)
+
