cd4d065acfbd7efd0f624348b16eecfb5ff72aa5
Krasi5 <krasi_todorova@yahoo.com>
2026-01-13 04:22:24 +0000

    urls created

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
-
 # Create your models here.
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -17,7 +17,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ["created_on"]
     
     def __str__(self):
         return f"{self.title} | written by {self.author}"
@@ -31,3 +31,10 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+   
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
 # Create your views here.
 
-def my_blog(request):
-    return HttpResponse("I am getting it")
+class PostList(generic.ListView):
+    model = Post
+
